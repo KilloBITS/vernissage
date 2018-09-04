be7df920d00167a32c113b415ebc6875667e8a39
mr.kalinuk <qazwsx159357>
2018-09-05 00:44:51 +0300

    Your Message

--- publick/script/index/index.js
+++ publick/script/index/index.js
@@ -16,9 +16,7 @@ var IndexInit = {
     }, 3500)
   },
   getCook: function (cookiename) {
-      // Get name followed by anything except a semicolon
       var cookiestring = RegExp("" + cookiename + "[^;]+").exec(document.cookie);
-      // Return everything after the equal sign, or an empty string if the cookie name not found
       return decodeURIComponent(!!cookiestring ? cookiestring.toString().replace(/^[^=]+./, "") : "");
   },
   loadUsers: function(data){
