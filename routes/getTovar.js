5deee4ebbe1bd1413c5919a9eeb5c822037c6c9b
mr.kalinuk <qazwsx159357>
2018-10-14 00:53:50 +0300

    Your Message

--- routes/getTovar.js
+++ routes/getTovar.js
@@ -6,17 +6,22 @@ const url = "mongodb://localhost:27017/"; //url from mongoDB dataBase
 
 router.get('/', function(req, res, next){
   mongoClient.connect(url, function(err, client){
-      const db = client.db("VERNISSAGE");
-      const config = db.collection("config");
-      const menu  = db.collection("menu");
+    const db = client.db("VERNISSAGE");
+    const config = db.collection("config");
+    const menu  = db.collection("menu");
+    const tovar  = db.collection("tovar");
 
-      if(err) return console.log(err);
+    if(err) return console.log(err);
 
      config.find().toArray(function(err, results_config){
        if(results_config[0].opens){
+
          menu.find().toArray(function(err, results_menu ){
-           res.render('tovar.ejs',{conf: results_config[0], menu: results_menu})
-           client.close();
+           tovar.find().toArray(function(err, results_tovar ){
+             res.render('tovar.ejs',{conf: results_config[0], menu: results_menu, tovarArr: results_tovar})
+             client.close();
+           });
+
          });
        }else{
          res.render('close.ejs')
