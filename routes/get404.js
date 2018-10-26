44d23cf99c1680740a918687a1aee5cac0da4e41
mrkalinuk <mr.kalinuk@gmail.com>
2018-10-26 22:30:49 +0300

    Your Message

--- routes/get404.js
+++ routes/get404.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const router = express.Router();
 const mongoClient = require("mongodb").MongoClient;
-// const url = "mongodb://localhost:27017/"; //url from mongoDB dataBase
 
 router.get('*', function(req, res, next){
   res.render('404.ejs')
