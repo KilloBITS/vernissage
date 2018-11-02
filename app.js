2da5e6e68fcb320114123aee7b5063a974ff7a43
mrkalinuk <mr.kalinuk@gmail.com>
2018-11-02 12:16:38 +0200

    Your Message

--- app.js
+++ app.js
@@ -123,134 +123,6 @@ app.post('/maxAImenu', maxAImenu);
 const removecategory = require('./controllers/admin/controllerMenu');
 app.post('/removecategory', removecategory);
 
-// /* GMAIL API*/
-// const readline = require('readline');
-// const {google} = require('googleapis');
-//
-// const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
-// const TOKEN_PATH = 'token.json';
-//
-// fs.readFile('credentials.json', (err, content) => {
-//   if (err) return console.log('Error loading client secret file:', err);
-//   authorize(JSON.parse(content), listLabels);
-// });
-//
-// function authorize(credentials, callback) {
-//   const {client_secret, client_id, redirect_uris} = credentials.installed;
-//   const oAuth2Client = new google.auth.OAuth2(
-//       client_id, client_secret, redirect_uris[0]);
-//
-//   // Check if we have previously stored a token.
-//   fs.readFile(TOKEN_PATH, (err, token) => {
-//     if (err) return getNewToken(oAuth2Client, callback);
-//     oAuth2Client.setCredentials(JSON.parse(token));
-//     callback(oAuth2Client);
-//   });
-// }
-//
-// function getNewToken(oAuth2Client, callback) {
-//   const authUrl = oAuth2Client.generateAuthUrl({
-//     access_type: 'offline',
-//     scope: SCOPES,
-//   });
-//   console.log('Authorize this app by visiting this url:', authUrl);
-//   const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-//   });
-//   rl.question('Enter the code from that page here: ', (code) => {
-//     rl.close();
-//     oAuth2Client.getToken(code, (err, token) => {
-//       if (err) return console.error('Error retrieving access token', err);
-//       oAuth2Client.setCredentials(token);
-//       // Store the token to disk for later program executions
-//       fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-//         if (err) return console.error(err);
-//         console.log('Token stored to', TOKEN_PATH);
-//       });
-//       callback(oAuth2Client);
-//     });
-//   });
-// }
-//
-// function listLabels(auth) {
-//   const gmail = google.gmail({version: 'v1', auth});
-//   gmail.users.labels.list({
-//     userId: 'me',
-//   }, (err, res) => {
-//     if (err) return console.log('The API returned an error: ' + err);
-//     const labels = res.data.labels;
-//     if (labels.length) {
-//       console.log('Labels:');
-//       labels.forEach((label) => {
-//         console.log(`- ${label.name}`);
-//       });
-//
-//     } else {
-//       console.log('No labels found.');
-//     }
-//   });
-//   getRecentEmail(auth);
-// }
-//
-// function getRecentEmail(auth) {
-//     const gmail = google.gmail({version: 'v1', auth});
-//     gmail.users.messages.list({auth: auth, userId: 'me', maxResults: 1,}, function(err, response) {
-//       console.log(response)
-//       //   if (err) {
-//       //       console.log('The API returned an error: ' + err);
-//       //       return;
-//       //   }
-//       //
-//       //
-//       // var message_id = response['data']['messages'][0]['id'];
-//       //
-//       // // Retreive the actual message using the message id
-//       // gmail.users.messages.get({auth: auth, userId: 'me', 'id': message_id}, function(err, response) {
-//       //     if (err) {
-//       //         console.log('The API returned an error: ' + err);
-//       //         return;
-//       //     }
-//       //
-//       //    console.log(response['data']);
-//       // });
-//     });
-// }
-// const ViberBot = require('viber-bot').Bot;
-// const BotEvents = require('viber-bot').Events;
-//
-// const bot = new ViberBot({
-// 	authToken: "48b39990fbe7d092-4a58ca694be85a6d-8f368a42bb2bdf5e",
-// 	name: "EchoBot",
-// 	avatar: "http://viber.com/avatar.jpg" // It is recommended to be 720x720, and no more than 100kb.
-// });
-//
-// // Perfect! Now here's the key part:
-// bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
-// 	// Echo's back the message to the client. Your bot logic should sit here.
-// 	response.send(message);
-// });
-//
-// // Wasn't that easy? Let's create HTTPS server and set the webhook:
-//
-// const port = process.env.PORT || 4010;
-//
-// // Viber will push messages sent to this URL. Web server should be internet-facing.
-//
-//
-// const TextMessage = require('viber-bot').Message.Text;
-//
-//
-//
-//
-// http.createServer(bot.middleware()).listen(port, () => {
-//   console.log("started viber");
-//   bot.getBotProfile().then(response => console.log(`Public Account Named: ${response.name}`));
-//
-//   // bot.sendMessage(userProfile, new TextMessage("Thanks for shopping with us"));
-// });
-//
-
 // app.listen(80, function(){
 //   global.baseName = 'VERNISSAGE';
 //   global.baseIP = 'mongodb://localhost:27017/';
@@ -258,29 +130,20 @@ app.post('/removecategory', removecategory);
 //   console.warn('STARTED VERNISSAGE SERVER ON PORT: 80');
 // });
 
-// const Nexmo = require('nexmo');
-// const nexmo = new Nexmo({
-//   apiKey: "a616dae5",
-//   apiSecret: "01oaiMhN0lsZ3L2g"
-// });
-//
-// const from = 'Nexmo'
-// const to = '380664273160'
-// const text = 'kuku'
-//
-// nexmo.message.sendSms(from, to, text)
-
 
 var options = {
-  key: fs.readFileSync('ssl/apache-selfsigned.key'),
-  cert: fs.readFileSync('ssl/ca-certificated.crt')
+  key: fs.readFileSync('./ssl/apache-selfsigned.key'),
+  cert: fs.readFileSync('./ssl/ca-certificates.crt')
 };
-app.listen(443, options, function(){
+
+https.createServer(options, (req, res) => {
   global.baseName = 'VERNISSAGE';
   global.baseIP = 'mongodb://localhost:27017/';
   global.online = 0;
-  console.warn('STARTED VERNISSAGE SERVER ON PORT: 443');
-})
+  console.warn('STARTED VERNISSAGE SERVER ON PORT: 80');
+}).listen(80);
+
+
 
 // http.createServer(app).listen(8000);
 // This line is from the Node.js HTTPS documentation.
